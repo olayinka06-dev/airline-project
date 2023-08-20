74c9cd6ccd04f3d73ce873a9f97105fa907bce06
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-08-20 09:29:11 -0700

    Adjusting the NavBar

--- src/components/entities/NavBar.jsx
+++ src/components/entities/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   return (
     <>
       <header
-        className={`border-t-[3px] bg-white sticky top-0 z-[100] border-solid border-text-orange ${
+        className={`border-t-[3px] bg-white z-[100] border-solid border-text-orange ${
           menu ? "" : "shadow-lg"
         } `}
       >
@@ -53,7 +53,7 @@ const NavBar = () => {
           </div>
           <div className="hidden md:flex items-center gap-3">
             <Link
-            onClick={() => setActiveLink("booking")}
+              onClick={() => setActiveLink("booking")}
               className={`bg-text-orange py-[10px] px-5 text-white rounded-2xl border-none`}
               href={"/booking"}
             >
@@ -69,8 +69,61 @@ const NavBar = () => {
             <div className="third"></div>
           </button>
         </nav>
+        <div className="w-full">
+          <div
+            className={`flex overflow-hidden z-[150]  w-full  transition-[all,3s,ease-linear] ${
+              menu ? "h-[40vh]" : "h-[0vh]"
+            } flex-col px-5 bg-white md:hidden absolute items-start gap-4`}
+          >
+            <Link
+              onClick={() => setActiveLink("home")}
+              className={`hover:text-text-orange ${
+                activeLink === "home" ? "text-text-orange" : ""
+              }`}
+              href={"/"}
+            >
+              Home
+            </Link>
+            <Link
+              onClick={() => setActiveLink("about")}
+              className={`hover:text-text-orange ${
+                activeLink === "about" ? "text-text-orange" : ""
+              }`}
+              href={"/about"}
+            >
+              About Us
+            </Link>
+            <Link
+              onClick={() => setActiveLink("services")}
+              className={`hover:text-text-orange ${
+                activeLink === "services" ? "text-text-orange" : ""
+              }`}
+              href={"/service"}
+            >
+              Services
+            </Link>
+            <Link
+              onClick={() => setActiveLink("track")}
+              className={`hover:text-text-orange ${
+                activeLink === "track" ? "text-text-orange" : ""
+              }`}
+              href={"/"}
+            >
+              Track Parcel
+            </Link>
+            <Link
+              onClick={() => setActiveLink("contact")}
+              className={`hover:text-text-orange ${
+                activeLink === "contact" ? "text-text-orange" : ""
+              }`}
+              href={"/contact"}
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
       </header>
-      <div className="w-full">
+      {/* <div className="w-full">
         <div
           className={`flex overflow-hidden z-[150]  w-full  transition-[all,3s,ease-linear] ${
             menu ? "h-[40vh]" : "h-[0vh]"
@@ -122,7 +175,7 @@ const NavBar = () => {
             Contact
           </Link>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
