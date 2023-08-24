a45828168dec9ff41b79e406b7037b79cf7a2a86
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-08-24 08:12:35 -0700

    creating the preloader for the homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,36 @@
+"use client";
 import Booking from "@/components/entities/Booking";
 import ContactInformation from "@/components/entities/ContactInformation";
+import Preloader from "@/components/entities/Preloader";
 import Testimonials from "@/components/entities/Testimonial";
 import AboutComponent from "@/components/home_components/AboutSection";
-import HeroSection from "@/components/home_components/Hero";
 import HeroBanner from "@/components/home_components/HeroBanner";
 import Service from "@/components/home_components/Service";
 import WhyChooseUs from "@/components/home_components/WhychooseUs";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000);
+  }, []);
   return (
     <>
-    {/* <HeroSection/> */}
-      <HeroBanner/>
-      <AboutComponent/>
-      <WhyChooseUs/>
-      <Service/>
-      <Booking/>
-      <Testimonials/>
-      <ContactInformation/>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <>
+          <HeroBanner />
+          <AboutComponent />
+          <WhyChooseUs />
+          <Service />
+          <Booking />
+          <Testimonials />
+          <ContactInformation />
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/entities/Preloader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader">
+        <div className="loader-inner"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
