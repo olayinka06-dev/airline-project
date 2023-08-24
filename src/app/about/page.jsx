e03de3b3b199aa9ef0a1a87f5c54f04c020ae67b
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-08-24 08:17:07 -0700

    creating the preloader for the aboutpage

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,23 +1,37 @@
+"use client";
 import HeroBanner from "@/components/entities/HeroBanner";
 import InteractiveFeatures from "@/components/about_components/InteractiveFeatures";
 import StatisticsNumbers from "@/components/about_components/StatisticsNumbers";
 import Testimonials from "@/components/entities/Testimonial";
 import AboutComponent from "@/components/home_components/AboutSection";
 import WhyChooseUs from "@/components/home_components/WhychooseUs";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Preloader from "@/components/entities/Preloader";
 
 const page = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
   return (
     <>
-      <HeroBanner
-        head={"Discover Our Story"}
-        paragraph={"Delivering Excellence in Transportation Services"}
-      />
-      <AboutComponent />
-      <StatisticsNumbers />
-      <WhyChooseUs />
-      <InteractiveFeatures />
-      <Testimonials />
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <>
+          <HeroBanner
+            head={"Discover Our Story"}
+            paragraph={"Delivering Excellence in Transportation Services"}
+          />
+          <AboutComponent />
+          <StatisticsNumbers />
+          <WhyChooseUs />
+          <InteractiveFeatures />
+          <Testimonials />
+        </>
+      )}
     </>
   );
 };
