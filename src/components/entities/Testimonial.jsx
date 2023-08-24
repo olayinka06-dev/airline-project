a799d71c2fb451a6609d574b1076600c02bb0069
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-08-24 03:50:35 -0700

    Ajusting the testimonial component

--- src/components/entities/Testimonial.jsx
+++ src/components/entities/Testimonial.jsx
@@ -1,31 +1,90 @@
-import React from 'react';
-import { FaQuoteLeft } from 'react-icons/fa';
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import SwiperCore, { Autoplay, Pagination } from "swiper/core";
+SwiperCore.use([Autoplay, Pagination]);
+SwiperCore.use([Autoplay, Pagination]);
+import { FaQuoteLeft } from "react-icons/fa";
 
-const Testimonials = () => {
+const Card = ({quote, image, author, position}) => {
+  return (
+    <div
+      className="bg-white p-6 rounded-lg shadow-md transform transition duration-300 hover:scale-105 hover:bg-blue-50"
+    >
+      <div className="flex items-start mb-4">
+        <FaQuoteLeft className="text-blue-500 text-2xl mr-2" />
+        <p className="text-gray-700 text-sm leading-relaxed">
+          {quote}
+        </p>
+      </div>
+      <div className="flex items-center mb-4">
+        <img
+          src={image}
+          alt="Testimonial"
+          className="w-10 h-10 rounded-full mr-3"
+        />
+        <div>
+          <p className="font-semibold text-gray-800">{author}</p>
+          <p className="text-gray-600">{position}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Testimonial = () => {
+  const breakpoints = {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  };
   const testimonials = [
     {
-      quote:
-        "TransportCompany has been an excellent partner for our logistics needs. Their timely and reliable services have helped our business run smoothly.",
+      quote: "TransportCompany has been an outstanding partner for our logistics needs. Their dedication to providing reliable and efficient transportation solutions has greatly benefited our business. We have complete trust in their team.",
       author: "Sarah Johnson",
       position: "Supply Chain Manager",
       image: "/images/testimonial-1.jpg",
     },
     {
-      quote:
-        "I've been using TransportCompany's services for years, and they never disappoint. Their dedicated team ensures that our goods are delivered safely and on time.",
+      quote: "I've had the pleasure of working with TransportCompany for several years, and I'm continually impressed by their commitment to excellence. Their timely deliveries and secure handling of our cargo have made them an essential part of our operations.",
       author: "Michael Smith",
       position: "Business Owner",
       image: "/images/testimonial-3.jpg",
     },
     {
-      quote:
-        "The professionalism and efficiency of TransportCompany are unparalleled. They handle our shipping and transportation needs with precision and care.",
+      quote: "The professionalism and efficiency of TransportCompany's services have had a significant positive impact on our company's logistics. Their attention to detail and customer satisfaction sets them apart from the competition.",
       author: "Emily White",
       position: "Operations Director",
       image: "/images/testimonial-4.jpg",
     },
+    {
+      quote: "TransportCompany consistently meets and exceeds our expectations. Their dedication to quality service and on-time deliveries makes them our preferred transportation partner. We value their expertise and reliability.",
+      author: "John Doe",
+      position: "Marketing Manager",
+      image: "/images/testimonial-1.jpg",
+    },
+    {
+      quote: "We rely on TransportCompany to handle our shipping needs, and they consistently deliver exceptional results. Their team's dedication to ensuring our cargo arrives safely and on schedule is unmatched.",
+      author: "Jessica Brown",
+      position: "Warehouse Supervisor",
+      image: "/images/testimonial-3.jpg",
+    },
+    {
+      quote: "TransportCompany's team has demonstrated a strong commitment to meeting our specific requirements. Their personalized approach and attention to detail have contributed to streamlining our logistics processes.",
+      author: "David Williams",
+      position: "Logistics Coordinator",
+      image: "/images/testimonial-4.jpg",
+    },
   ];
-
   return (
     <section className="py-20 bg-gray-100">
       <div className="container mx-auto">
@@ -33,37 +92,35 @@ const Testimonials = () => {
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-gray-800 text-center">
           What Our Clients Say
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {testimonials.map((testimonial, index) => (
-            <div
-              key={index}
-              className="bg-white p-6 rounded-lg shadow-md transform transition duration-300 hover:scale-105 hover:bg-blue-50"
-            >
-              <div className="flex items-start mb-4">
-                <FaQuoteLeft className="text-blue-500 text-2xl mr-2" />
-                <p className="text-gray-700 text-lg leading-relaxed">
-                  {testimonial.quote}
-                </p>
-              </div>
-              <div className="flex items-center mb-4">
-                <img
-                  src={testimonial.image}
-                  alt={`Testimonial from ${testimonial.author}`}
-                  className="w-10 h-10 rounded-full mr-3"
-                />
-                <div>
-                  <p className="font-semibold text-gray-800">
-                    {testimonial.author}
-                  </p>
-                  <p className="text-gray-600">{testimonial.position}</p>
-                </div>
-              </div>
-            </div>
-          ))}
+        <div className="w-full">
+          <Swiper
+            modules={[Autoplay, Pagination]}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={breakpoints}
+          >
+            {
+              testimonials.map((testimonial, i)=>(
+                <SwiperSlide key={i}>
+              <Card
+               image={testimonial.image}
+               author={testimonial.author}
+               quote={testimonial.quote}
+               position={testimonial.position}
+              />
+            </SwiperSlide>
+              ))
+            }
+          </Swiper>
         </div>
       </div>
     </section>
   );
 };
 
-export default Testimonials;
+export default Testimonial;
--- src/components/home_components/HeroBanner.jsx
+++ src/components/home_components/HeroBanner.jsx
@@ -25,12 +25,13 @@ const HeroBanner = () => {
           }}
         >
           <SwiperSlide>
-            <div className="flex justify-center text-white items-center w-full bg-[url(/images/4.jpg)] bg-center z-[-100] h-[90vh] bg-no-repeat">
-              <div className="container mb-4">
-                <h1 className="md:text-[50px] text-[30px] text-text-orange font-bold">
+          <div className="absolute inset-0 bg-[rgb(41,108,161)] opacity-60"></div>
+            <div className="z-[-10]  flex justify-center text-white items-center w-full bg-[url(/images/4.jpg)] bg-center  h-[90vh] bg-no-repeat">
+              <div className="absolute top-48 inset-0 container mb-4">
+                <h1 className=" md:text-[50px] z-[300] text-[30px] text-text-orange font-bold">
                 Reliable and Efficient Transportation Services
                 </h1>
-                <p className="text-[24px] leading-relaxed">
+                <p className="text-[24px] z-[300] leading-relaxed">
                 Count on us for dependable and streamlined transportation solutions that cater to your needs, ensuring your goods are delivered promptly and with utmost efficiency.
                 </p>
               </div>
@@ -75,6 +76,74 @@ const HeroBanner = () => {
         </Swiper>
         <div className="swiper-button-next"></div>
         <div className="swiper-button-prev"></div>
+        <style jsx>{`
+          .swiper-slide-next,
+          .swiper-slide-prev {
+            opacity: 0;
+            pointer-events: none;
+          }
+          
+          .swiper-button-next,
+          .swiper-button-prev {
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            background-color: rgb(250, 70, 18);
+            color: white !important;
+            font-size: 14px !important;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            z-index: 10;
+            padding: 15px 20px;
+            border-radius: 10px;
+            animation: buttonnext 3s linear infinite;
+            opacity: 1;
+          }
+          @media screen and (max-width: 950px) {
+            .swiper-button-next,
+            .swiper-button-prev {
+              display: none !important;
+            }
+          }
+          .swiper-button-next {
+            right: 20px;
+          }
+          @keyframes buttonnext {
+            0% {
+              transform: translateX(10px);
+              opacity: 0.8;
+            }
+            50% {
+              transform: translateX(0px);
+              opacity: 1;
+            }
+            100% {
+              transform: translateX(10px);
+              opacity: 0.8;
+            }
+          }
+          
+          .swiper-button-prev {
+            left: 20px;
+          }
+          
+          .swiper-button-prev::after,
+          .swiper-button-next::after {
+            font-size: 10px !important;
+          }
+          .swiper-pagination-bullet-active {
+            background-color: rgb(250, 70, 18) !important;
+            padding: 5px 20px;
+            border-radius: 5px !important;
+          }
+          
+          .swiper-pagination-bullet:not(.swiper-pagination-bullet-active) {
+            padding: 5px 5px;
+            background-color: #242424 !important;
+          }
+        `}</style>
       </div>
     </div>
   );
